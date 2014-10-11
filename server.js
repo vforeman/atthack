67bc6ca38c466dd527f57fcbeda377b9f5497303
victor foreman <vforeman@scs.howard.edu>
2014-10-11 00:02:49 -0400

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	app = express();
+
+app.use(express.static(__dirname));
+app.get('/',function(req,res){
+	res.sendfile('index.html');
+});
+
+app.listen(5000,function(){
+	console.log('listening on port 5000');
+});
